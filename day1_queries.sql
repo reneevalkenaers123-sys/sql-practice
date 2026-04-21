87401a15fdb173c656a3381c5124c456bbba1e2c
Renee Valkenaers <reneevalkenaers123@gmail.com>
2026-04-21 14:40:05 +0200

    Create day1_queries.sql

---
+++ day1_queries.sql
@@ -0,0 +1,6 @@
+SELECT * FROM customers;
+SELECT name FROM customers;
+SELECT * FROM customers WHERE country = 'Belgium';
+SELECT DISTINCT name FROM customers;
+SELECT * FROM customers ORDER BY name;
+SELECT * FROM customers LIMIT 2;
