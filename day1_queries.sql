5f8ed322c8100101f9cba151b1313f5a4a0fec0d
Renee Valkenaers <reneevalkenaers123@gmail.com>
2026-04-23 17:38:48 +0200

    Update day1_queries.sql

--- day1_queries.sql
+++ day1_queries.sql
@@ -1,6 +1,22 @@
+USE bootcamp;
+
+-- BASIC SELECT
 SELECT * FROM customers;
+
 SELECT name FROM customers;
-SELECT * FROM customers WHERE country = 'Belgium';
-SELECT DISTINCT name FROM customers;
-SELECT * FROM customers ORDER BY name;
-SELECT * FROM customers LIMIT 2;
+
+-- WHERE
+SELECT * FROM customers
+WHERE country = 'Belgium';
+
+-- DISTINCT
+SELECT DISTINCT name
+FROM customers;
+
+-- ORDER BY
+SELECT * FROM customers
+ORDER BY name;
+
+-- LIMIT
+SELECT * FROM customers
+LIMIT 2;
